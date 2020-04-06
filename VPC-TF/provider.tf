87f719185937c23056bdf7c1c1d64f7d9753df11
etapemejame <etapeemblek@yahoo.com>
2020-04-06 11:00:09 -0400

    Update provider.tf

--- VPC-TF/provider.tf
+++ VPC-TF/provider.tf
@@ -1,7 +1,4 @@
 provider "aws" {
-  //access_key = "AKIA4DRU3MMEJYOSQ3XB"
-  //secret_key = "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"
   region     = "us-east-1"
-  shared_credentials_file = "C:/Users/etape/.aws/credentials"
-  //profile                 = "%USERPROFILE%"
+  shared_credentials_file = "C:/Users/terra-user/.aws/credentials"
 }
